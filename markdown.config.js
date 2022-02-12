8b5f4fb72f8a4b2020bfe2724e0239e0f1f41d07
Julien Polo <julien.polo@koober.com>
2022-02-12 23:36:09 +0100

    feat: init repository

---
+++ markdown.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable unicorn/prefer-module */
+/* eslint-disable global-require */
+module.exports = {
+  callback() {
+    // console.log('done')
+  },
+  transforms: {
+    PKGJSON: require('markdown-magic-package-json'),
+    SUBPACKAGELIST: require('markdown-magic-subpackage-list'),
+  },
+};
