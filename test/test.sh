b922c9c0f4f9bf60a893fa5eef6dddd53985a876
Julien Polo <julien.polo@gmail.com>
2026-03-02 14:55:17 +0100

    🔧 Migrate to pnpm

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,6 @@ preset_files=$(grep -lri --exclude-dir=node_modules --include="*.json" "https://
 
 for preset_file in $preset_files
 do
-  RENOVATE_CONFIG_FILE="$preset_file" npm exec -- renovate-config-validator
+  RENOVATE_CONFIG_FILE="$preset_file" renovate-config-validator
 done
 
