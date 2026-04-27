5c46aaab8f0e5407821e0734e2fb67aa5397c0a3
Julien Polo <julien.polo@gmail.com>
2026-04-27 14:59:44 +0200

    Squashed '.modules/makefile-ci/' changes from 51184cc..935009a
    935009a Add package manager installation step for asdf in NodeJS setup
    
    git-subtree-dir: .modules/makefile-ci
    git-subtree-split: 935009a19ce15b7f7d6d60a380d7c216fc280b8e

--- src/node.mk
+++ src/node.mk
@@ -148,6 +148,14 @@ $(NODEJS_CACHE_PATH)/node-version: $(NODEJS_CACHE_PATH)
 NODEJS_PACKAGE_JSON_FILES := $(shell find . -name package.json -not -path './node_modules/*' -not -path './.git/*')
 
 $(NODEJS_STATEFILE): $(wildcard $(NODEJS_LOCKFILE)) $(NODEJS_PACKAGE_JSON_FILES)
+# Try installing package manager
+ifeq ($(filter npm,$(NODEJS_PACKAGE_MANAGER)),)
+# Only for asdf we have to reshim after corepack
+	$(Q)if ! $(NODEJS_PACKAGE_MANAGER_COMMAND) -v &>/dev/null; then \
+		$(call log,info,"[NodeJS] Install $(NODEJS_PACKAGE_MANAGER)...",1); \
+		$(.NODEJS_INSTALL_PACKAGE_MANAGER_COMMAND); \
+	fi
+endif
 	@$(call log,info,"[NodeJS] Ensure dependencies....",1)
 	$(Q)${NODEJS_INSTALL}
 	$(Q)${TOUCH} $@
